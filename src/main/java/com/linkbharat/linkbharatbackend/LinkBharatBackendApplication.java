3cb0ff5e10189d7863fd4ed2133aee2dd19c3688
= <maheshkadambala18@gmail.com>
2025-10-07 15:07:35 +0530

    initial code

---
+++ src/main/java/com/linkbharat/linkbharatbackend/LinkBharatBackendApplication.java
@@ -0,0 +1,13 @@
+package com.linkbharat.linkbharatbackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LinkBharatBackendApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(LinkBharatBackendApplication.class, args);
+    }
+
+}
